ed56b00b21f3bd3c7ec2c9079c7cf955ff29d8b0
radare <pancake@nopcode.org>
2011-12-13 02:26:07 +0100

    add package description file

---
+++ package.lua
@@ -0,0 +1,8 @@
+return {
+  name = "connect",
+  version = "0.1",
+  description = "Node's Connect like HTTP server framework",
+  author = "pancake <pancake@nopcode.org>",
+  dependencies = {
+  },
+}
